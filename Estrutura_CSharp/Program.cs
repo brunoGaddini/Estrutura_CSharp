50602b58802e0a388cc62b890d272acd6ed0d4ad
bruno_gaddini <bruno_gaddini@hotmail.com>
2021-12-03 01:54:20 -0300

    Adicionar arquivos de projeto.

---
+++ Estrutura_CSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Estrutura_CSharp;
+using System; 
+
+namespace Estrutura_CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var s = new Pilhas();
+            s.Empilha(1);
+            s.Empilha(10);
+            s.Empilha(100);
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+        }
+    }
+}
+
+
+
